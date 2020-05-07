f5190790dd7e53116f8b105844cddd40f9188abe
Kimmy Andersson <cyaspik@gmail.com>
2020-05-07 23:21:54 +0200

    Added async examples

--- NSonic.ExampleConsoleApp/Program.cs
+++ NSonic.ExampleConsoleApp/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace NSonic.ExampleConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var hostname = "localhost";
             var port = 1491;
@@ -20,18 +21,27 @@ namespace NSonic.ExampleConsoleApp
                     var info = control.Info();
                     Console.WriteLine($"INFO: {info}");
 
+                    info = await control.InfoAsync();
+                    Console.WriteLine($"INFO (async): {info}");
+
                     control.Trigger("consolidate");
                 }
 
                 using (var search = NSonicFactory.Search(hostname, port, secret))
                 {
-                    search.Connect();
+                    await search.ConnectAsync();
 
                     var queryResults = search.Query("messages", "user:1", "s");
                     Console.WriteLine($"QUERY: {string.Join(", ", queryResults)}");
 
+                    queryResults = await search.QueryAsync("messages", "user:1", "s");
+                    Console.WriteLine($"QUERY (async): {string.Join(", ", queryResults)}");
+
                     var suggestResults = search.Suggest("messages", "user:1", "s");
                     Console.WriteLine($"SUGGEST: {string.Join(", ", suggestResults)}");
+
+                    suggestResults = await search.SuggestAsync("messages", "user:1", "s");
+                    Console.WriteLine($"SUGGEST (async): {string.Join(", ", suggestResults)}");
                 }
 
                 using (var ingest = NSonicFactory.Ingest(hostname, port, secret))
@@ -43,17 +53,32 @@ namespace NSonic.ExampleConsoleApp
                     var popResult = ingest.Pop("messages", "user:1", "conversation:1", "This is an example push.");
                     Console.WriteLine($"POP: {popResult}");
 
+                    popResult = await ingest.PopAsync("messages", "user:1", "conversation:1", "This is an example push.");
+                    Console.WriteLine($"POP (async): {popResult}");
+
                     var countResult = ingest.Count("messages", "user:1");
                     Console.WriteLine($"COUNT: {countResult}");
+
+                    countResult = await ingest.CountAsync("messages", "user:1");
+                    Console.WriteLine($"COUNT (async): {countResult}");
 
                     var flushCollectionResult = ingest.FlushCollection("messages");
                     Console.WriteLine($"FLUSHC: {flushCollectionResult}");
 
+                    flushCollectionResult = await ingest.FlushCollectionAsync("messages");
+                    Console.WriteLine($"FLUSHC (async): {flushCollectionResult}");
+
                     var flushBucketResult = ingest.FlushBucket("messages", "user:1");
                     Console.WriteLine($"FLUSHB: {flushBucketResult}");
 
+                    flushBucketResult = await ingest.FlushBucketAsync("messages", "user:1");
+                    Console.WriteLine($"FLUSHB (async): {flushBucketResult}");
+
                     var flushObjectResult = ingest.FlushObject("messages", "user:1", "conversation:1");
                     Console.WriteLine($"FLUSHO: {flushObjectResult}");
+
+                    flushObjectResult = await ingest.FlushObjectAsync("messages", "user:1", "conversation:1");
+                    Console.WriteLine($"FLUSHO (async): {flushObjectResult}");
                 }
 
                 Console.WriteLine("Press any key to exit...");
