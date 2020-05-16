2c9c53ef96c2d0b3e9223271783d7ab943aefe83
Kimmy Andersson <cyaspik@gmail.com>
2020-05-16 18:49:12 +0200

    Open coverage results after local run

--- cover.cmd
+++ cover.cmd
@@ -5,5 +5,6 @@ PUSHD NSonic.Tests
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
 REM Previous line will generate coverage.cobertura.xml
 tools\reportgenerator.exe -reports:coverage.cobertura.xml -targetdir:coverage -reporttypes:Html
+explorer coverage\index.htm
 
 POPD
