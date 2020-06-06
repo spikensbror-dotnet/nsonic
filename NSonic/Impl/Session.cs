dd0889a003548a500f543ba1e36ff1f23b0e4c78
Kimmy Andersson <cyaspik@gmail.com>
2020-06-06 22:40:25 +0200

    Separate LockingSession to its own file.

--- NSonic/Impl/Session.cs
+++ NSonic/Impl/Session.cs
@@ -1,53 +1,10 @@
-﻿using NSonic.Impl.Net;
-using NSonic.Utils;
-using System;
-using System.Collections.Generic;
+﻿using NSonic.Utils;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace NSonic.Impl
 {
-    class LockingSession : ISession
-    {
-        private readonly ISession session;
-        private readonly IClient client;
-
-        public LockingSession(ISession session, IClient client)
-        {
-            this.session = session;
-            this.client = client;
-            this.client.Semaphore.Wait();
-        }
-
-        public void Dispose()
-        {
-            this.client.Semaphore.Release();
-            this.session.Dispose();
-        }
-
-        public string Read()
-        {
-            return this.session.Read();
-        }
-
-        public Task<string> ReadAsync()
-        {
-            return this.session.ReadAsync();
-        }
-
-        public void Write(params string[] args)
-        {
-            this.session.Write(args);
-        }
-
-        public Task WriteAsync(params string[] args)
-        {
-            return this.session.WriteAsync(args);
-        }
-    }
-
     class Session : ISession
     {
         public Session(IClient client)
